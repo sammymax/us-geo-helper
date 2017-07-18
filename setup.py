58454c4c97c5747c0a477cd2a2ee810443e5efc3
Philip Sun <philwsun@gmail.com>
2017-07-18 00:27:32 -0400

    Add travis, license, setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup
 exec(open('us-geo-helper/version.py').read())
 setup(
-	name = 'us-geo-helper',
-	packages = ['us-geo-helper'], # this must be the same as the name above
-	version = __version__,
-	description = 'Python wrapper for US Census Data',
-	author = 'Philip Sun',
-	author_email = 'traders@mit.edu',
-	license = 'MIT',
-	url = 'https://github.com/sammymax/us-geo-helper',
-	download_url = 'https://github.com/sammymax/us-geo-helper/tarball/' + __version__,
-	install_requires = ['numpy', 'pandas'],
-	keywords = ['geography'],
-	classifiers = [],
+    name = 'us-geo-helper',
+    packages = ['us-geo-helper'], # this must be the same as the name above
+    version = __version__,
+    description = 'Python wrapper for US Census Data',
+    author = 'Philip Sun',
+    author_email = 'traders@mit.edu',
+    license = 'MIT',
+    url = 'https://github.com/sammymax/us-geo-helper',
+    download_url = 'https://github.com/sammymax/us-geo-helper/tarball/' + __version__,
+    install_requires = ['numpy', 'pandas'],
+    keywords = ['geography'],
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
 )
